07538b5e6877695c59643cf8299373df68a36e91
Chaitanya Chandrakant Gorule <chaitanyagorule05@gmail.com>
2023-06-13 23:00:08 +0530

    division wala inside dev

--- Main.java
+++ Main.java
@@ -2,6 +2,6 @@ public class Main {
     int a= 4;
     int b= 5;
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(a/b);
     }
 }
